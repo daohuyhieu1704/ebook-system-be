4731b7a3848030626cb9e6785ab035a770e381a0
HP\daohu <daohuyhieu1704@gmail.com>
2024-02-02 11:42:41 +0700

    access controller

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,7 +1,6 @@
 class AccessController {
   signUp = async (req, res, next) => {
     try {
-      console.log("AccessController.signUp", req.body);
       let { authorization } = req.headers;
       const isAuthen = mylib.verifyAuthorizationEmp(
         authorization.split(" ")[1]
@@ -22,6 +21,47 @@ class AccessController {
             error: false,
             message: "Success",
           });
+        }
+      );
+    } catch (e) {
+      next(error);
+    }
+  };
+  signIn = async (req, res, next) => {
+    try {
+      var body = JSON.parse(req.body);
+      const username = body.username.replace(/[;'"-]/g, "");
+      const pw = body.password.replace(/[;'"-]/g, "");
+      const passwordHashed = saltedSha256(pw, env.SALT_PASSWORD);
+      connection.query(
+        "SELECT username,hoten,email,role FROM user_admin where username = '" +
+          username +
+          "' and password = '" +
+          passwordHashed +
+          "'",
+        (err, resRows) => {
+          if (err) throw err;
+          if (resRows.length == 0) {
+            res.json({
+              state: false,
+              message: "Sai thông tin đăng nhập",
+            });
+          } else {
+            const dataRow = mylib.parseToJSONFrDB(resRows)[0];
+            const actNew = mylib.generAccessTokenEmp(
+              username,
+              passwordHashed,
+              dataRow.role
+            );
+
+            res.json({
+              state: true,
+              message: "Đăng nhập thành công.",
+              fullname: dataRow.fullname,
+              access_token: actNew,
+              role: parseInt(dataRow.role),
+            });
+          }
         }
       );
     } catch (e) {
--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -4,5 +4,6 @@ const accessController = require("../../controllers/access.controller");
 const router = express.Router();
 
 router.post("/shop/signup", accessController.signUp);
+router.post("/shop/signin", accessController.signIn);
 
 module.exports = router;
