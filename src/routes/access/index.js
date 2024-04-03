1f6aeca8a821b5b0c1acd30f8105203a0e080914
HP\daohu <daohuyhieu1704@gmail.com>
2024-04-03 08:45:27 +0700

    HieuDh: update something

--- src/routes/access/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const accessController = require("../../controllers/access.controller");
-
-const router = express.Router();
-
-// router.post("/shop/sign-up", accessController.signUp);
-// router.post("/shop/sign-in", accessController.signIn);
-    
-export default router;
--- src/routes/authen/index.js
+++ src/routes/authen/index.js
@@ -6,4 +6,6 @@ const router = express.Router();
 router.post("/shop/login", new AuthenController().postLogin);
 router.post("/shop/refresh-token", new AuthenController().postRefreshToken);
 
+router.post("/shop/logout", new AuthenController().postLogout);
+
 export default router;
