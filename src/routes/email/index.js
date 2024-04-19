367384c50e203cbae07fb7ee9f3f8884628cf1cf
HP\daohu <daohuyhieu1704@gmail.com>
2024-04-19 16:56:28 +0700

    HieuDh: destroy token

--- src/routes/email/index.js
+++ src/routes/email/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 // User's functions
 router.post("/new-template", new EmailController().newTemplate);
+router.delete("/destroy-token", new EmailController().destroyToken);
 
 export default router;
